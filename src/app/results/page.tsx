e7d0379531d7c86e587eca81ed4dc069ca5590eb
yashhshah12 <161322746+yashhshah12@users.noreply.github.com>
2026-04-08 19:02:50 +0530

    feat: implement results page UI with fetch data

---
+++ src/app/results/page.tsx
@@ -0,0 +1,56 @@
+
+import {fetcherData} from '../../utils/fetchers';
+   export const dynamic = 'force-dynamic';
+   import styles from './results.module.css';
+    const ResultPage  = async ({searchParams} : {
+        searchParams : {q? : string}
+    })=>{
+        const query =  searchParams.q || 'Biology';
+
+        const papers = await fetcherData(query);
+            console.log(papers);
+     
+        return (
+                <>
+                <main className={styles.container}>
+                    <div className={styles.heading}>
+                <h1 className={styles.title}>
+                 Results for: <span className="">"{query}"</span>
+                </h1>
+                <input type="text" className={styles.searchpaper}  value={query}/>
+                </div>
+                <div className={styles.cardList}>
+                {
+                    papers.map((paper)=> (
+                        <div className={styles.card} key={paper.id}>
+                        <div className={styles.cardHeader}>
+                            <span className={styles.sourceBadge}> {paper.source}</span>
+                            <span className={styles.data}>  {paper.publishDate}</span>
+                        </div>
+                        <h2 className={styles.header}>{paper.title}</h2>
+                        <p >{paper.abstract}</p>
+                        <p className={styles.author}>{paper.author}</p>
+                        <a href={paper.url}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        >
+                            View original paper
+                        </a>
+                        </div>
+                    ))
+                }
+                </div>
+
+
+                </main>
+                </>
+
+
+
+        )
+
+
+    }
+
+    export default ResultPage
+
