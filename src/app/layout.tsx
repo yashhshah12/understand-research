0aa1879d1094977695f9f115d956ec58febf6437
yashhshah12 <161322746+yashhshah12@users.noreply.github.com>
2026-06-28 16:15:21 +0530

    feat: Integrate Login and Sign up button in UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import {AuthLinks} from '../components/AuthLinks';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,9 +22,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const showAuthlinks = process.env.NEXT_PUBLIC_SHOW_AUTH === 'true';
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body className="min-h-dv">
+        {showAuthlinks && (
+      <AuthLinks /> 
+        )
+        }
+        
+        {children}</body>
     </html>
   );
 }
---
+++ src/components/AuthLinks.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+const AuthLinks  = ()=>{
+
+
+
+    return (
+        <>
+        <div className='flex justify-end gap-3 pt-3 max-w-[80rem]  max-[800]:mr-3 '>
+        <Link href={''} className='no-underline p-2 hover:bg-[oklch(48.8%_0.243_264.376)] hover:text-white rounded-[10px] active:translate-y-0.5 transition-all duration-300 ease-in-ou  '>
+        Log In
+        </Link>
+        <Link href={''} className='no-underline p-2 bg-[#2563eb] rounded-[10px] text-white hover:bg-[oklch(48.8%_0.243_264.376)] active:translate-y-0.5 transition-all duration-300 ease-in-ou'>
+            Sign Up         
+       </Link>
+        </div>
+
+    
+    
+        </>
+    )
+}
+export {AuthLinks}
