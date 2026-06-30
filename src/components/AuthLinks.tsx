8971564821b1c96db5cf606180d519586fe0b26d
yashshah <yashhshah12@gmail.com>
2026-06-30 19:03:11 +0530

    fix: Create a pth in authlink tab for sigup

--- src/components/AuthLinks.tsx
+++ src/components/AuthLinks.tsx
@@ -9,7 +9,7 @@ const AuthLinks  = ()=>{
         <Link href={`/login`} className='no-underline p-2 hover:bg-[oklch(48.8%_0.243_264.376)] hover:text-white rounded-[10px] active:translate-y-0.5 transition-all duration-300 ease-in-ou  '>
         Log In
         </Link>
-        <Link href={''} className='no-underline p-2 bg-[#2563eb] rounded-[10px] text-white hover:bg-[oklch(48.8%_0.243_264.376)] active:translate-y-0.5 transition-all duration-300 ease-in-ou'>
+        <Link href={'/signup'} className='no-underline p-2 bg-[#2563eb] rounded-[10px] text-white hover:bg-[oklch(48.8%_0.243_264.376)] active:translate-y-0.5 transition-all duration-300 ease-in-ou'>
             Sign Up         
        </Link>
         </div>
