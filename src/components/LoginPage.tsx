f9d95967bce20fd6986ca73a05a1ed69a1b5ed4a
yashshah <yashhshah12@gmail.com>
2026-06-30 18:45:04 +0530

    feat: Completed login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -12,13 +12,13 @@ const LoginPage = ()=>{
             <form action="" className="flex flex-col gap-7 mt-5 mb-20 ">
             <input type="email" placeholder="Email address"  className="py-3 px-2 rounded-[15px] text-[1.3rem] border-gray-300"  />
             <input type="password" name="" id="" placeholder="Password" className="py-3 px-2 rounded-[15px] text-[1.3rem] border-gray-300"/>
-           <Link href={''} className='text-right text-[1.3rem] text-blue-600' >
+           <Link href={''} className='text-right text-[1.3rem] text-blue-600 no-underline' >
             Forget password
            
            </Link>
-                <button type='submit' className='bg-[#2563eb]  text-center px-2 py-3 rounded-[15px] text-[1.3rem] border-none border-gray-300" no-underline text-white font-sans'>Log In</button>
+                <button type='submit' className='bg-[#2563eb]  text-center px-2 py-3 rounded-[15px] text-[1.3rem] border-none border-gray-300"  text-white font-sans'>Log In</button>
             
-            <Link href={''} className='text-center text-[1.3rem] text-blue-600 max-[400]:flex flex-col'> <span>Don't have an account?  </span>
+            <Link href={''} className='text-center text-[1.3rem] text-blue-600 max-[400]:flex flex-col no-underline' > <span>Don't have an account?  </span>
                 
                 <span> Sign up </span>
           
